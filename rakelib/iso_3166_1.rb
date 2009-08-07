c9ab8788e6eb0016ad4825c783d4386d86fb7a48
Alex Rabarts <alexrabarts@gmail.com>
2009-08-07 09:45:13 +0100

    Hpricot -> Nokogiri

--- rakelib/iso_3166_1.rb
+++ rakelib/iso_3166_1.rb
@@ -1,4 +1,4 @@
-require 'hpricot'
+require 'nokogiri'
 require 'open-uri'
 require 'erubis'
 
@@ -6,24 +6,31 @@ class IsoCountryCodes
   module Task
     module UpdateCodes
       def self.get
-        doc   = Hpricot(open('http://en.wikipedia.org/wiki/ISO_3166-1').read)
-        codes = {}
+        doc    = Nokogiri::HTML.parse(open('http://en.wikipedia.org/wiki/ISO_3166-1'))
+        codes  = {}
         td_map = {
-          :name    => 0,
-          :numeric => 1,
-          :alpha3  => 2,
-          :alpha2  => 3
+          :name    => 1,
+          :alpha2  => 2,
+          :alpha3  => 3,
+          :numeric => 4
         }
+
         code_labels = td_map.keys
-        doc.search('.wikitable.sortable:first tr').each do |row|
+
+        doc.search('table.wikitable.sortable tr').each do |row|
           value_hash = {}
+
           code_labels.each do |key|
             selector = "td:nth-of-type(#{td_map[key]})"
             selector << ' a' if key == :name
-            value = row.search(selector).inner_text.strip
+
+            value = row.search(selector).text.strip
+
             next if value == ''
+
             value_hash[key] = value
-            if value_hash.length == code_labels.length
+
+            if value_hash.length == code_labels.length - 1
               value_hash.keys.each do |value_hash_key|
                 codes[value_hash[value_hash_key]] = value_hash if value_hash_key == :alpha3
               end
