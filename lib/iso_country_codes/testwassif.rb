4441a2e8d83085a93d2ec5e3e27c4bb8831eb52d
Wassif Aziz <wassifaziz@gmail.comgit config --global user.email wassifaziz@gmail.comgit config --global github.user wassifaziz@gmail.com>
2012-04-27 11:22:53 +0100

    deleted unwanted files

--- lib/iso_country_codes/testwassif.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'rubygems'
-require '/Users/awassif1/o2_projects/iso_country_codes/lib/iso_country_codes.rb'
-
-country_code_enter =("36") #example
-code = IsoCountryCodes.find(country_code_enter)
-puts country_code_enter
-puts code.name
