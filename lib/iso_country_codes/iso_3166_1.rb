cbeb393e77290b3517b96119eaf49291c0080141
Alex Rabarts <alexrabarts@gmail.com>
2013-01-28 16:30:31 +0000

    Latest ISO 3166 changes from Wikipedia

--- lib/iso_country_codes/iso_3166_1.rb
+++ lib/iso_country_codes/iso_3166_1.rb
@@ -214,7 +214,7 @@ class IsoCountryCodes
     end
     class MKD < Code #:nodoc:
       self.numeric = %q{807}
-      self.name    = %q{Macedonia, the former Yugoslav Republic of}
+      self.name    = %q{Macedonia, The Former Yugoslav Republic of}
       self.alpha2  = %q{MK}
       self.alpha3  = %q{MKD}
     end
