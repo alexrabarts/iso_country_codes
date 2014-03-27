5714bd820479fbc678c2ad35cce9547e37b04fb1
Marcello Barnaba <vjt@openssl.it>
2014-03-27 18:13:15 +0100

    Move all our patches in a separate file cc:@lleirborras

--- lib/iso_country_codes/iso_3166_1.rb
+++ lib/iso_country_codes/iso_3166_1.rb
@@ -95,7 +95,7 @@ class IsoCountryCodes
     class SRB < Code #:nodoc:
       self.numeric = %q{688}
       self.name    = %q{Serbia}
-      self.alpha2  = %q{YG}#%q{RS} IFAD WRONG CODE
+      self.alpha2  = %q{RS}
       self.alpha3  = %q{SRB}
     end
     class AZE < Code #:nodoc:
@@ -425,7 +425,7 @@ class IsoCountryCodes
     class COD < Code #:nodoc:
       self.numeric = %q{180}
       self.name    = %q{Congo, the Democratic Republic of the}
-      self.alpha2  = %q{ZR}#%q{CD} IFAD WRONG CODE
+      self.alpha2  = %q{CD}
       self.alpha3  = %q{COD}
     end
     class ETH < Code #:nodoc:
@@ -935,7 +935,7 @@ class IsoCountryCodes
     class TLS < Code #:nodoc:
       self.numeric = %q{626}
       self.name    = %q{Timor-Leste}
-      self.alpha2  = %q{TP}#%q{TL} IFAD WRONG CODE
+      self.alpha2  = %q{TL}
       self.alpha3  = %q{TLS}
     end
     class NRU < Code #:nodoc:
