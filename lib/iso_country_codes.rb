57e4abb21d1a07fcbba80067650b305f48096d71
Grey Baker <greysteil@gmail.com>
2015-04-23 13:48:35 +0100

    Add IBAN details from ISO 13616-1

--- lib/iso_country_codes.rb
+++ lib/iso_country_codes.rb
@@ -3,6 +3,7 @@ $LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
 
 require 'iso_country_codes/code'
 require 'iso_country_codes/iso_3166_1'
+require 'iso_country_codes/iso_13616_1'
 require 'iso_country_codes/iso_4217'
 require 'iso_country_codes/calling'
 require 'iso_country_codes/continent'
